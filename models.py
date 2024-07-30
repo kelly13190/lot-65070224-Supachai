c76f5e33ca30391adb43380995511021e420eec4
Supachai Maneerat <65070224@kmitl.ac.th>
2024-07-31 00:11:09 +0700

    .

--- models.py
+++ models.py
@@ -3,21 +3,40 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 
 from database import Base
 
-class Book(Base): # คือการสร้างคลาส Book โดยสืบทอดคุณสมบัติจาก Base
-    __tablename__ = 'books' # คือการกำหนดชื่อตารางในฐานข้อมูล
+class Book(Base):
+    __tablename__ = 'books'
 
-    id = Column(Integer, primary_key=True, index=True) # คือการกำหนดฟิลด์ id โดยเป็น primary key
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     author = Column(String, index=True)
     year = Column(Integer, index=True)
     is_published = Column(Boolean, index=True)
+    detail = Column(String, index=True)
+    synopsis = Column(String, index=True)
+    category = Column(String, index=True)
+
+class Coffee(Base):
+    __tablename__ = 'coffees'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+    price = Column(Integer, index=True)
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True, index=True, unique=True, autoincrement=True)
+    coffee_id = Column(Integer, ForeignKey('coffees.id'))
+    quantity = Column(Integer, index=True)
+    total_price = Column(Integer, index=True)
+    notes = Column(String, index=True)
 
 class Student(Base):
     __tablename__ = 'students'
 
-    student_id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     firstname = Column(String, index=True)
     lastname = Column(String, index=True)
-    birth_day = Column(String, index=True)
-    age = Column(Integer, index=True)
-    sex = Column(String, index=True)
+    std_id = Column(Integer, index=True)
+    birth = Column(String, index=True)
+    gender = Column(String, index=True)
